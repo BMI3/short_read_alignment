30e6052258f67b0a2e4611e78d68bbf952441026
ianguang38 <54056962+ianguang38@users.noreply.github.com>
2021-12-12 15:07:33 +0800

    save the visualization as png

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import time
 import matplotlib.pyplot as plt
+import sys
 import logging
 import ag2
 import ag_tra
@@ -50,33 +51,43 @@ def agt(s):
     ag_tra.fl(s)
 
 
-def test_function():
+def ag2_vs_agt(iter, show=False):
     time_cost = []
-    try:
-        for i in range(2, 5):
-            print(i)
-            s = generator.generate_DNA(10 ** i)
-            time0 = time.time()
-            ag2.BWTstore(s)
-            time1 = time.time()
-            ag_tra.fl(s)
-            time2 = time.time()
-            time_cost.append((time1 - time0, time2 - time1))
-            print(time1 - time0, time2 - time1)
-    except MemoryError:
-        print("memoryError at i=", i)
-    # fig, ax = plt.subplots(1, 2)
-    # ax[0].plot(range(2, 5), [time_cost[i][0] for i in range(len(time_cost))])
-    # ax[1].plot(range(2, 5), [time_cost[i][1] for i in range(len(time_cost))])
+    for i in iter:
+        print(i)
+        s = generator.generate_DNA(i)
+        time0 = time.time()
+        ag2.BWTstore(s)
+        time1 = time.time()
+        ag_tra.fl(s)
+        time2 = time.time()
+        tc = time1 - time0, time2 - time1
+        time_cost.append(tc)
+        print(tc)
     nn = len(time_cost)
+    plt.xscale("log")
     plt.plot(
-        range(2, 2 + nn),
+        iter,
         [time_cost[i][0] for i in range(nn)],
-        range(2, 2 + nn),
+        iter,
         [time_cost[i][1] for i in range(nn)],
     )
-    plt.show()
+    if show:
+        plt.show()
+    else:
+        plt.savefig(str(iter) + ".png")
+
+
+def test_function():
+    logs = [10 ** i for i in range(2, 8)]
+    ag2_vs_agt(logs)
+
+    ag2_vs_agt(range(10 ** 4, 8 * 10 ** 4, 10 ** 4))
+    ag2_vs_agt(range(10 ** 4, 2 * 10 ** 4, 10 ** 3))
+    ag2_vs_agt(range(16 * 10 ** 3, 17 * 10 ** 3, 10 ** 2))
+    ag2_vs_agt(range(163 * 10 ** 2, 164 * 10 ** 2, 10))
 
 
 if __name__ == "__main__":
     test_function()
+    # print(sys.platform == "win32")
